d10388447ab3753fd6ff39eec03d2da225138c93
Allekstat <alexsander.santana2@etec.sp.gov.br>
2024-11-24 00:48:17 -0300

    svg

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const menu = document.getElementById("menu");
 
+function abrir_e_fechar_menu(clique) {
+    if(clique.isTrusted) {
+        clique.preventDefault();
+        menu.classList.toggle("menu");
+    }
+}
+
+menu.addEventListener("click", abrir_e_fechar_menu(event));
+
+//menu.addEventListener("touchstart", abrir_e_fechar_menu(event));
